cd995aeec44ff6c736dae963966d231e6b63ed5a
sid <sidbhasin82@gmail.com>
2020-10-31 10:59:46 +0530

    Initial Commit

---
+++ parallel-and-concurrent-programming/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  parallel-and-concurrent-programming
+//
+//  Created by Siddharth Bhasin on 31/10/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
