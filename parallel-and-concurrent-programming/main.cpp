7270ac816e2ab98037f12b9cbed516fbc2abf584
sid <sidbhasin82@gmail.com>
2020-10-31 11:24:44 +0530

    Thread vs process demo

--- parallel-and-concurrent-programming/main.cpp
+++ parallel-and-concurrent-programming/main.cpp
@@ -5,10 +5,27 @@
 //  Created by Siddharth Bhasin on 31/10/20.
 //
 
-#include <iostream>
+/**
+ * Threads that waste CPU cycles
+ */
+#include <thread>
+#include <chrono>
+#include <unistd.h>
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
-	return 0;
+// a simple function that wastes CPU cycles "forever"
+void cpu_waster() {
+    printf("CPU Waster Process ID: %d\n", getpid());
+    printf("CPU Waster Thread ID %d\n", std::this_thread::get_id());
+    while(true) continue;
+}
+
+int main() {
+    printf("Main Process ID: %d\n", getpid());
+    printf("Main Thread ID: %d\n", std::this_thread::get_id());
+    std::thread thread1(cpu_waster);
+    std::thread thread2(cpu_waster);
+
+    while(true) { // keep the main thread alive "forever"
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 }
